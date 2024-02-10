022998b612af52c17543b467c8c857168ad3147f
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-02-09 20:16:01 -0500

    add under construction page

---
+++ src/components/MyApps.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppsItem from './AppsItem.vue'
+import GroupIcon from './icons/IconGroup.vue'
+</script>
+
+<template>
+  <AppsItem>
+    <template #icon>
+      <GroupIcon />
+    </template>
+    <template #heading>Forum App</template>
+    <a href="https://forum.chinh.ca/" target="_blank" rel="noopener">...in development >>></a>
+  </AppsItem>
+</template>
