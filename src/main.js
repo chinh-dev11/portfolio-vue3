fe3e8429c3ba7c90666f7b4ba7a432ddca982780
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-02-18 12:04:04 -0500

    add link fontawesome icon

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 
-createApp(App).mount('#app')
+import fontAwesome from '@/plugins/FontAwesome'
+
+const app = createApp(App)
+
+// plugins
+app.use(fontAwesome)
+
+app.mount('#app')
